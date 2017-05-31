580c93294a9f02168d2171b7d8e4ccd791876e94
liyinqing <yinqing_521@126.com>
2017-05-31 23:24:12 +0800

    网络音频显示

--- app/src/main/java/atguigu/com/mobilvideo321/paper/RecyclerNetAudioFragment.java
+++ app/src/main/java/atguigu/com/mobilvideo321/paper/RecyclerNetAudioFragment.java
@@ -1,10 +1,17 @@
 package atguigu.com.mobilvideo321.paper;
 
+import android.os.Handler;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
+import com.cjj.MaterialRefreshLayout;
+import com.cjj.MaterialRefreshListener;
+
+import atguigu.com.mobilvideo321.R;
 import atguigu.com.mobilvideo321.fragment.BaseFragment;
+
 
 public class RecyclerNetAudioFragment extends BaseFragment {
 
@@ -16,10 +23,83 @@ public class RecyclerNetAudioFragment extends BaseFragment {
 
     private TextView textView;
     private RecyclerView recyclerView;
+    private MaterialRefreshLayout refresh;
 
     @Override
     public View initView() {
-      return null;
+        View view = View.inflate(context, R.layout.recycler_view, null);
+        refresh = (MaterialRefreshLayout) view.findViewById(R.id.refresh);
+        textView = (TextView) view.findViewById(R.id.tv_nodata);
+        recyclerView = (RecyclerView) view.findViewById(R.id.net_recycler_audio);
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        refresh.setMaterialRefreshListener(new MaterialRefreshListener() {
+            @Override
+            public void onRefresh(MaterialRefreshLayout materialRefreshLayout) {
+                isRefresh = true;
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        getDataFromNet();
+                    }
+                }, 2000);
+
+            }
+
+            @Override
+            public void onRefreshLoadMore(MaterialRefreshLayout materialRefreshLayout) {
+                super.onRefreshLoadMore(materialRefreshLayout);
+                isRefresh = false;
+                getMoreData();
+            }
+        });
+//        adapter = new MultipleAdapter(mContext, datas);
+//        recyclerView.setAdapter(adapter);
+//        adapter.setOnItemClickListener(new MultipleAdapter.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(View view, int position) {
+//                NetAudioBean.ListBean listEntity = adapter.getItem(position);
+//                if (listEntity != null) {
+//                    Intent intent = new Intent(mContext, ShowImageAndGifActivity.class);
+//                    if (listEntity.getType().equals("gif")) {
+//                        String url = listEntity.getGif().getImages().get(0);
+//                        intent.putExtra("url", url);
+//                        mContext.startActivity(intent);
+//                    } else if (listEntity.getType().equals("image")) {
+//                        String url = listEntity.getImage().getBig().get(0);
+//                        intent.putExtra("url", url);
+//                        mContext.startActivity(intent);
+//                    }
+//
+//                }
+//            }
+//        });
+      return view;
+    }
+    private void getDataFromNet() {
+//        RequestParams request = new RequestParams(NET_AUDIO_URL);
+//        x.http().get(request, new ItemTouchHelper.Callback.CommonCallback<String>() {
+//            @Override
+//            public void onSuccess(String result) {
+//                Log.e("TAG", "onSuccess--result---" + result);
+//                parseJson(result);
+//                refresh.finishRefresh();
+//            }
+//
+//            @Override
+//            public void onError(Throwable ex, boolean isOnCallback) {
+//                Toast.makeText(mContext, "onerror-----" + ex.getStackTrace(), Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onCancelled(CancelledException cex) {
+//
+//            }
+//
+//            @Override
+//            public void onFinished() {
+//
+//            }
+//        });
     }
 
 
@@ -28,7 +108,34 @@ public class RecyclerNetAudioFragment extends BaseFragment {
     public void initData() {
         super.initData();
     }
-
+    private void getMoreData() {
+//        String newUrl = LAST_URL + count + NEXT_URL;
+//        RequestParams request = new RequestParams(newUrl);
+//        x.http().get(request, new Callback.CommonCallback<String>() {
+//            @Override
+//            public void onSuccess(String result) {
+//                Log.e("TAG", "onSuccess--result---" + result);
+//                parseJson(result);
+//                refresh.finishRefreshLoadMore();
+//                count += 20;
+//            }
+//
+//            @Override
+//            public void onError(Throwable ex, boolean isOnCallback) {
+//                Toast.makeText(mContext, "onerror-----" + ex.getStackTrace(), Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onCancelled(CancelledException cex) {
+//
+//            }
+//
+//            @Override
+//            public void onFinished() {
+//
+//            }
+//        });
+    }
 
 
 
